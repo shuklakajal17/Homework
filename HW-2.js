8a39466753e61003349616cd180eace4df387477
kajal shukla <shuklakajal1703@gmail.com>
2025-02-11 12:44:44 +0530

    Update HW-2.js

--- HW-2.js
+++ HW-2.js
@@ -1,10 +1,17 @@
-let str = "k \nr \ni \ns \nh \nn \na";
-let newStr = [];
+// let str = "k \nr \ni \ns \nh \nn \na";
+// let newStr = [];
 
-for(let i = str.length-1; i >= 0; i--){
-    newStr += str[i];
-}
+// for(let i = str.length-1; i >= 0; i--){
+//     newStr += str[i];
+// }
 
-console.log(newStr);
+// console.log(newStr);
 
 // "k", "r", "i", "s", "h", "n", "a"
+
+
+let str = "k r i s h n a";
+
+for (let i = str.length - 1; i >= 0; i--) {
+  console.log(str[i]);
+}
