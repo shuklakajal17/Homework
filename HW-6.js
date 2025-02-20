6d2ba2ef0348ecd7732dfe95d222a016a237c2ef
kajal shukla <shuklakajal1703@gmail.com>
2025-02-20 13:46:49 +0530

    Add files via upload

---
+++ HW-6.js
@@ -0,0 +1,9 @@
+let num1 = 3;
+let num2 = 12;
+
+
+for (let i = 0; i <= 11; i++){
+    console.log(i * num1);
+    console.log(i * num2);
+}
+
---
+++ HW-7.js
@@ -0,0 +1,18 @@
+let arr1 = [1, 2, 3, 4];
+let sum1 = 0;
+
+
+for(let i = 0; i < arr1.length; i++){
+    sum1 += arr1[i];
+}
+console.log(sum1);
+
+
+
+let arr2 = [1, 2, 3, 4, 5, 6, 7];
+let sum2 = [0];
+
+for(let i = 0; i < arr2.length; i++){
+    sum2 += arr2[i];
+}
+console.log(sum2);
