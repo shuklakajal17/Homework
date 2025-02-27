cc716c836c9df4a1c61804b3b4ca9632461a6aa9
kajal shukla <shuklakajal1703@gmail.com>
2025-02-28 02:01:59 +0530

    Add files via upload

---
+++ HW-12.js
@@ -0,0 +1,9 @@
+function myNumber(num){
+    if(num % 2 == 0){
+        console.log("even");
+    }else{
+        console.log("odd");
+    }
+}
+myNumber(10);
+myNumber(7);
---
+++ HW-13.js
@@ -0,0 +1,13 @@
+myVote(20);
+myVote(16);
+
+
+
+
+function myVote(age){
+    if(age >= 18){
+        console.log("eligible to vote");
+    }else{
+        console.log("not eligiable");
+    }
+}
