e72a9b679d66bcea43dbb07132b55edac78601f3
kajal shukla <shuklakajal1703@gmail.com>
2025-02-28 00:31:50 +0530

    Add files via upload

---
+++ HW-11.js
@@ -0,0 +1,15 @@
+myNumber(5);
+myNumber(-3);
+myNumber(0);
+
+
+
+function myNumber(num){
+    if (num > 0){
+        console.log("positive");
+    }else if (num < 0){
+        console.log("negative");
+    }else{
+        console.log("zero");
+    }
+}
