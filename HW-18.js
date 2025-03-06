710c8682d89a92341479e5737e73869341a8cd69
kajal shukla <shuklakajal1703@gmail.com>
2025-03-06 14:12:45 +0530

    Update HW-18.js

--- HW-18.js
+++ HW-18.js
@@ -1,13 +1,19 @@
-personHeight(180);
-personHeight(165);
+// personHeight(180);
+// personHeight(165);
 
 
 
 
-function personHeight(height){
-    if(height >= 170){
-        console.log("tall");
-    }else{
-        console.log("short");
-    }
-}
+// function personHeight(height){
+//     if(height >= 170){
+//         console.log("tall");
+//     }else{
+//         console.log("short");
+//     }
+// }
+
+
+const height = 180;
+const result = height >= 170 ? 'tall' : 'short';
+
+console.log(result);
