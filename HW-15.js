9e6778e10f6d817ea96d01da71a4c16ad3c0f7e7
kajal shukla <shuklakajal1703@gmail.com>
2025-03-06 14:11:03 +0530

    Update HW-15.js

--- HW-15.js
+++ HW-15.js
@@ -1,13 +1,21 @@
-function myGrade(marks){
-    if(marks >= 90){
-        console.log("A");
-    }else if (marks >= 80){
-        console.log("B");
-    }else if(marks >= 70){
-        console.log("C");
-    }else {
-        console.log("D");
-    }
-}
-myGrade(85);
-myGrade(72);
+// function myGrade(marks){
+//     if(marks >= 90){
+//         console.log("A");
+//     }else if (marks >= 80){
+//         console.log("B");
+//     }else if(marks >= 70){
+//         console.log("C");
+//     }else {
+//         console.log("D");
+//     }
+// }
+// myGrade(85);
+// myGrade(72);
+
+const score = 85;
+
+const result = score >= 90 ? 'A' :
+(score >= 80 ? 'B' :
+(score >= 70 ? 'C' : 'D'));
+
+console.log(result);
