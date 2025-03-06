57a098b9239d815e0b6be89637b5a187d96a6ad4
kajal shukla <shuklakajal1703@gmail.com>
2025-03-06 14:11:35 +0530

    Update HW-16.js

--- HW-16.js
+++ HW-16.js
@@ -1,16 +1,23 @@
-classifyTemperature(10);
-classifyTemperature(22);
-classifyTemperature(35);
+// classifyTemperature(10);
+// classifyTemperature(22);
+// classifyTemperature(35);
 
 
 
 
-function classifyTemperature(degree){
-    if(degree <= 15){
-        console.log("cold");
-    }else if(degree <= 25){
-        console.log("moderate");
-    }else{
-        console.log("hot");
-    }
-}
+// function classifyTemperature(degree){
+//     if(degree <= 15){
+//         console.log("cold");
+//     }else if(degree <= 25){
+//         console.log("moderate");
+//     }else{
+//         console.log("hot");
+//     }
+// }
+
+
+const temp = 22;
+
+const result = temp <= 15 ? 'cold' :
+(temp <= 30 ? 'moderate' : 'hot');
+console.log(result);
