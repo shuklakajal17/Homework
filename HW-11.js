67bf2187246088070cffa2f8e862c4ae5108456e
kajal shukla <shuklakajal1703@gmail.com>
2025-03-06 14:08:03 +0530

    Update HW-11.js

--- HW-11.js
+++ HW-11.js
@@ -1,15 +1,21 @@
-myNumber(5);
-myNumber(-3);
-myNumber(0);
+// myNumber(5);
+// myNumber(-3);
+// myNumber(0);
 
 
 
-function myNumber(num){
-    if (num > 0){
-        console.log("positive");
-    }else if (num < 0){
-        console.log("negative");
-    }else{
-        console.log("zero");
-    }
-}
+// function myNumber(num){
+//     if (num > 0){
+//         console.log("positive");
+//     }else if (num < 0){
+//         console.log("negative");
+//     }else{
+//         console.log("zero");
+//     }
+// }
+
+const number = 5;
+const result = number > 0 ? 'positive' : (number < 0 ? 'negative' : 'zero');
+
+console.log(result);
+
