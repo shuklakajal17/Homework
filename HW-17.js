f1cb92e822a0b9233afe6880d5cd64c01507dc50
kajal shukla <shuklakajal1703@gmail.com>
2025-03-06 14:12:16 +0530

    Update HW-17.js

--- HW-17.js
+++ HW-17.js
@@ -1,12 +1,17 @@
-divisibleNumber(55);
-divisibleNumber(23);
+// divisibleNumber(55);
+// divisibleNumber(23);
 
 
 
-function divisibleNumber(num){
-    if(num % 5 == 0){
-        console.log("divisible");
-    }else{
-        console.log("not divisible");
-    }
-}
+// function divisibleNumber(num){
+//     if(num % 5 == 0){
+//         console.log("divisible");
+//     }else{
+//         console.log("not divisible");
+//     }
+// }
+
+const num = 55;
+const result = num % 5 === 0 ? 'divisible' : 'not divisible';
+
+console.log(result);
