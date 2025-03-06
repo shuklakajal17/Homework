81b4eeb45e4b058ed4f03814e5476884234cc28f
kajal shukla <shuklakajal1703@gmail.com>
2025-03-06 14:08:58 +0530

    Update HW-12.js

--- HW-12.js
+++ HW-12.js
@@ -1,9 +1,15 @@
-function myNumber(num){
-    if(num % 2 == 0){
-        console.log("even");
-    }else{
-        console.log("odd");
-    }
-}
-myNumber(10);
-myNumber(7);
+// function myNumber(num){
+//     if(num % 2 == 0){
+//         console.log("even");
+//     }else{
+//         console.log("odd");
+//     }
+// }
+// myNumber(10);
+// myNumber(7);
+
+
+const num = 10;
+const result = num % 2 === 0 ? 'even' : 'odd';
+
+console.log(result);
